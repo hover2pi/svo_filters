78d985bc4076cb40c1fcfcbddd3f4c5021058ef9
Joe Filippazzo <jfilippazzo@stsci.edu>
2017-05-19 14:07:53 -0400

    Initial code commit.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+from codecs import open
+from os import path
+
+setup(
+    name='svo_filters',
+    version='0.1.0',
+    description='A Python wrapper for the SVO Filter Profile Service',
+    url='https://github.com/hover2pi/svo_filters.git',
+    author='Joe Filippazzo',
+    author_email='jfilippazzo@stsci.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    keywords='astrophysics',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy','astropy','matplotlib'],
+
+)
---
+++ svo_filters/__init__.py
@@ -0,0 +1,5 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+A Python wrapper for the SVO Filter Profile Service
+"""
+from .svo import *
