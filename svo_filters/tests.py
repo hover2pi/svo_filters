85be482d252f4e26d733b1a09a20dab3471eed3a
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-07-31 14:47:34 -0400

    Switched from pytest to unittest

--- svo_filters/tests.py
+++ svo_filters/tests.py
@@ -1,20 +1,29 @@
 """Some tests to make sure the filters work as intended"""
-
+import unittest
 import astropy.units as q
-from .svo import Filter
+from svo_filters.svo import Filter
 
-def test_filter_class():
-    """Test if Filter object is created properly"""
-    filt = Filter('2MASS.J')
-    
-    is_filter = isinstance(filt, Filter)
-    
-    assert is_filter
-    
-def test_tophat():
-    """Test if Filter object is created properly"""
-    filt = Filter('tophat', wl_min=0.8*q.um, wl_max=1.2*q.um)
-    
-    is_filter = isinstance(filt, Filter)
-    
-    assert is_filter
+ 
+class TestFilters(unittest.TestCase):
+    """Tests for Filter class"""
+    def setUp(self):
+        pass
+
+    def test_filter_class(self):
+        """Test if Filter object is created properly"""
+        filt = Filter('2MASS.J')
+
+        is_filter = isinstance(filt, Filter)
+
+        self.assertTrue(is_filter)
+
+    def test_tophat(self):
+        """Test if Filter object is created properly"""
+        filt = Filter('tophat', wl_min=0.8*q.um, wl_max=1.2*q.um)
+
+        is_filter = isinstance(filt, Filter)
+
+        self.assertTrue(is_filter)
+
+if __name__ == '__main__':
+    unittest.main()
