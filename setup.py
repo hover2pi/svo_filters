706d7bddcd62470adc352a7862170428df48f0fc
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-07-31 15:52:28 -0400

    Imports

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     keywords='astrophysics',
     packages=find_packages(exclude=['contrib', 'docs']),
-    package_data={'svo_filters':['data/*','data/filters/*']},
+    package_data={'svo_filters': ['data/*', 'data/filters/*', 'data/spectra/*']},
     include_package_data=True,
     install_requires=['numpy','astropy','matplotlib'],
 
