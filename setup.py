09f10d1dd1c7a4e74861f00c1c50fbabd6607281
Joe Filippazzo <jfilippazzo@stsci.edu>
2019-02-21 15:33:30 -0500

    0.2.19

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 
 setup(
     name='svo_filters',
-    version='0.2.18',
+    version='0.2.19',
     description='A Python wrapper for the SVO Filter Profile Service',
     long_description='A Python wrapper for the SVO Filter Profile Service',
     url='https://github.com/hover2pi/svo_filters',
